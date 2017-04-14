a069f9c0d67018cfc2c0af7bfcfcab4f78c41c7c
neiljphilip <nphilip33@yahoo.com>
2017-04-14 00:42:39 -0400

    hello world

--- kkarp.c
+++ kkarp.c
@@ -1,21 +1,11 @@
 //
 // kkarp.c
-//
-<<<<<<< HEAD
 // Created by 90982966 and 90977369
 //
 
-
-
-
-int main() {
-    
-}
-=======
 #include <stdio.h>
 
 int main() {
     printf("Hello World!");
     return 0;
 }
->>>>>>> 472fd6f221b13a67adca571eca23d994949a5a2d
